5179732c07da231d83ebabd1104c1895482b4ec4
Igaro-Franklin <igarofranklin@gmail.com>
2024-12-17 16:10:13 -0300

    Iniciou o projeto mobile

---
+++ mobile/app/step/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import { colors } from '../../constants/colors';
+
+export default function Step(){
+    return(
+        <View>
+            <Text>
+                Teste de pagina!
+            </Text>
+        </View>
+    )
+}
